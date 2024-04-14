109e10640c5ff077ca101517fd041839830948bb
felipejoq <felipejofreq@gmail.com>
2024-04-13 23:03:05 -0400

    Creación de artículos

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,14 +3,26 @@ import AxiosInterceptor from "../utils/api/axios.api.js";
 export class AuthService {
 
   static async getUserById({id}) {
-    return AxiosInterceptor.get(`/api/v1/user/${id}`);
+    try {
+      return AxiosInterceptor.get(`/api/v1/user/${id}`);
+    } catch (error) {
+      return error;
+    }
   }
 
   static async login({email, password}) {
-    return AxiosInterceptor.post('/api/v1/user/login', {email, password});
+    try {
+      return AxiosInterceptor.post('/api/v1/user/login', {email, password});
+    } catch (error) {
+      return error;
+    }
   }
 
   static async register({name, email, password}) {
-    return AxiosInterceptor.post('/api/v1/user/register', {name, email, password});
+    try {
+      return AxiosInterceptor.post('/api/v1/user/register', {name, email, password});
+    } catch (error) {
+      return error;
+    }
   }
 }
